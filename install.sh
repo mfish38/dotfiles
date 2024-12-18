f66ee14f5a93a4070b6bc3056ec652d1d63fee4d
mfish38 <markdfisher38@gmail.com>
2024-12-18 11:06:45 -0500

    better venv

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
-sudo apt update
 
 function pkg() {
     echo "Installing: $@"
@@ -16,7 +15,7 @@ function install_font() {
 
     local font_zip=$(basename $1)
 
-    if ! [ -f $font_zip ]; then
+    if ! [ -f "$font_zip" ]; then
         curl -LO $1
         unzip $font_zip
 
@@ -26,6 +25,25 @@ function install_font() {
 
     popd
 }
+
+function ensure_venv(){
+    if ! [ -d "$HOME/.venvs/$1" ]; then
+        pushd ~/.venvs
+
+        python3 -m venv "$1"
+
+        popd
+    fi
+
+    source "$HOME/.venvs/$1/bin/activate"
+
+    python3 -m pip install --upgrade ${@:2}
+
+    deactivate
+}
+
+
+sudo apt update
 
 # Setup user bin folder. Note that on Ubuntu this will be on the path.
 # Note that if it did not exist, a re-login will be needed before it is added to the path
@@ -54,16 +72,8 @@ fi
 mkdir ~/.venvs
 pkg python3-pip python3-venv
 
-if ! [ -f ~/.venvs/py3nvim ]; then
-    pushd ~/.venvs
-    python3 -m venv py3nvim
-    source ~/.venvs/py3nvim/bin/activate
-
-    python3 -m pip install --upgrade pynvim
-
-    deactivate
-    popd
-fi
+ensure_venv py3nvim \
+    pynvim
 
 # LazyVim deps
 pkg clang fd-find fzf chafa ripgrep lua5.1 luarocks cargo
