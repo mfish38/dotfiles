dce0c5917e86e63c307cc79798510006b9738b3f
mfish38 <markdfisher38@gmail.com>
2025-01-07 10:33:03 -0500

    update plugins and disable auto triger copilot

---
+++ nvim/.config/nvim/lua/plugins/plugins.lua
@@ -0,0 +1,14 @@
+return {
+  { "echasnovski/mini.pairs", enabled = false },
+  {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    opts = {},
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      auto_trigger = false,
+    },
+  },
+}
