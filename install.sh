1551747e13cca3b96516448a3a14347760290794
mfish38 <markdfisher38@gmail.com>
2025-02-12 16:18:44 -0500

    Added sublime merge

--- install.sh
+++ install.sh
@@ -393,6 +393,22 @@ fi
 
 pkg wezterm
 
+# Sublime Merge
+
+# Get the gpg key if it is not already present.
+if ! [ -f /etc/apt/trusted.gpg.d/sublimehq-archive.gpg ]; then
+    wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg >/dev/null
+fi
+
+pkg apt-transport-https
+
+if ! [ -f /etc/apt/sources.list.d/sublime-text.list ]; then
+    echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+    sudo apt-get update
+fi
+
+pkg sublime-merge
+
 # Stow
 pushd ~/.dotfiles || exit
 stow nvim
